3469fc8b3ab108b25c418c72ddfa339205270833
Huang.CK <549596429@qq.com>
2018-10-25 10:00:22 +0800

    update

---
+++ widget/libs/iview/Layout/Sider.js
@@ -0,0 +1,14 @@
+import Widget from '../../../base/Widget'
+
+export default class Sider extends Widget {
+    constructor() {
+        super(
+            `Sider`,
+            `iview`,
+            `Sider`
+        )
+        this.style = {
+            "minHeight": "10px"
+        }
+    }
+}
