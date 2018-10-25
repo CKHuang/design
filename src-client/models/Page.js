41d9bd3e561e3e6d0323117fffe880bf454e281c
Huang.CK <549596429@qq.com>
2018-10-25 09:54:57 +0800

    update: remove Project model name

---
+++ src-client/models/Page.js
@@ -0,0 +1,40 @@
+import Http from '../libs/Http'
+import httpConfig from '../../config/http'
+
+/**
+ * @description 
+ */
+export default class PageModel extends Http {
+
+    constructor() {
+        super();
+        this.baseURL = httpConfig.baseURL
+    }
+
+    /**
+     * @description 查询项目的key
+     * @param {string} projectKey 项目的key 
+     */
+    async queryProjectPages(projectKey) {
+        return await this.request(
+            `GET`,
+            this.merge(this.baseURL,`/project/${projectKey}/page`)
+        )
+    }
+
+    async insert(projectKey,inserted) {
+        return await this.request(
+            `POST`,
+            this.merge(this.baseURL,`/project/${projectKey}/page`),
+            {data:inserted}
+        )
+    }
+
+    async update(pid,updated) {
+        return await this.request(
+            `PUT`,
+            this.merge(this.baseURL,`/page/${pid}`),
+            {data:updated}
+        )
+    }
+}
